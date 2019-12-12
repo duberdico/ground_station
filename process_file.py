7be8205b8bff0304191c06529b60e9b8fb223663
Ricardo Antunes <ricardo@solcast.com>
2019-12-12 10:50:10 +0000

    some edits to process_file.py

--- process_file.py
+++ process_file.py
@@ -6,6 +6,21 @@ import numpy as np
 import xarray as xr
 import argparse
 import logging
+import datetime as dt
+
+
+def parse_filename(fname):
+    fname = os.path.split(fname)[-1].replace(".CS16", "")
+    parts = fname.split("_")
+    for i, s in enumerate(parts):
+        if (len(s) == 8) and (len(parts[i + 1]) == 6):
+            time = dt.datetime.strptime(s + parts[i + 1], "%Y%m%d%H%M%S")
+        elif "fs" in s:
+            fs = int(s.replace("fs", ""))
+        elif "Hz" in s:
+            f0 = int(s.replace("Hz", ""))
+
+    return (time, fs, f0)
 
 
 def normalize_complex_arr(a):
@@ -15,23 +30,36 @@ def normalize_complex_arr(a):
 
 def make_spectrogram(filename, fs, fc, nfft):
 
-    data = np.memmap(
-        filename, dtype=np.dtype([("i", np.int16), ("q", np.int16)]), mode="r"
-    )
-    cdata = data["i"] + data["q"] * 1j
-    cdata = normalize_complex_arr(cdata)
+    xa = []
+    with open(filename, "rb") as fid:
+        while True:
+            data = np.fromfile(
+                fid,
+                dtype=np.dtype([("i", np.int16), ("q", np.int16)]),
+                count=100 * nfft,
+            )
+            if len(data) == 0:
+                break
+            cdata = data["i"] + data["q"] * 1j
+            cdata = normalize_complex_arr(cdata)
+            Sxx, f, t, im = plt.specgram(cdata, NFFT=nfft, Fs=fs, Fc=fc, noverlap=0)
+            t_step = t[1] - t[0]
+            da.append(
+                xr.DataArray(
+                    data=Sxx,
+                    dims=["Frequency", "Time"],
+                    coords={"Frequency": f, "Time": lt + t_step * 0.5 + t},
+                )
+            )
+            t_step = t[1] - t[0]
+            lt = t[-1]
 
-    f, t, Sxx = sgn.spectrogram(
-        cdata, fs=fs, noverlap=None, nfft=nfft, return_onesided=False
-    )
-    da = xr.DataArray(
-        data=Sxx,
-        dims=["Frequency", "Time"],
-        coords={"Frequency": f, "Time": t}
-    )
+    da = xr.concat(da, dim="Time")
     da.attrs = {"nfft": nfft, "fs": fs, "fc": fc}
     da["Frequency"].attrs = {"Units": "Hz"}
     da["Time"].attrs = {"Units": "s"}
+
+    return da
 
 
 if __name__ == "__main__":
@@ -39,6 +67,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("-l", "--log_file", metavar="N", type=str, help="logging file")
     parser.add_argument("-f", "--file", metavar="N", type=str, help="recording file")
+    parser.add_argument("--nfft", metavar="N", type=int, help="nfft", default=2048)
     parser.add_argument(
         "-v", "--verbose", help="increase output verbosity", action="store_true"
     )
@@ -54,7 +83,8 @@ if __name__ == "__main__":
     )
 
     filename = args.file
-    file_params = par
-    make_spectrogram(filename, fs, fc, nfft)
+    nfft = args.nfft
+    time, fs, fc = parse_filename(filename)
+    da = make_spectrogram(filename, fs, fc, nfft)
 
     sys.exit(main())
